959cbd3a936605b0d8a33b6223a85f357f5d09e1
argraur <me@argraur.dev>
2023-10-13 19:05:03 +0300

    Initial app architecture

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MORE.Tech VTB Map"
+include(":app")
+include(":core:network")
+include(":features:auth")
+include(":features:map")
+include(":data:auth")
+include(":data:offices")
